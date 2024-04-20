35031cb4256b056da6d51ad84a04b018d959da2f
Deokgi-Park <ejrrl6931@gmail.com>
2024-04-20 10:03:14 +0900

    backup

--- src/rbtree.c
+++ src/rbtree.c
@@ -5,6 +5,17 @@
 rbtree *new_rbtree(void) {
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
   // TODO: initialize struct if needed
+  // nil 노드 초기화 진행
+  node_t *inNode = calloc(1,sizeof(node_t));
+  inNode->color = 0;
+  inNode->parent = NULL;
+  inNode->left = NULL;
+  inNode->right = NULL;
+  
+  // root, nill 초기화
+  p->root=NULL;
+  p->nil=inNode;
+  
   return p;
 }
 
@@ -15,6 +26,58 @@ void delete_rbtree(rbtree *t) {
 
 node_t *rbtree_insert(rbtree *t, const key_t key) {
   // TODO: implement insert
+  // key 셋팅할 노드 생성
+  node_t *inNode = calloc(1,sizeof(node_t));
+  // 트리 순회 노드 생성
+  node_t *cur;
+  inNode->key = key;
+  inNode->color = 0;
+  inNode->parent = t->nil;
+  inNode->left = NULL;
+  inNode->right = NULL;
+
+  //루트가 없는 경우 루트 설정
+  if(t->root == NULL){
+    t->root = inNode;
+    //루트는 무조건 검정 : 1
+    t->root->color = 1;
+    
+    return t->root;
+  }
+
+  // 현재 노드 설정
+  cur = t->root;
+  while(1){
+    if(cur->key > inNode->key){
+      if(cur->left == NULL){
+        inNode->parent = cur;
+        cur->left = inNode;
+        cur = cur->left;
+        break;
+      }
+      cur = cur->left;
+    }
+    else{
+      if(cur->right == NULL){
+        inNode->parent = cur;
+        cur->right = inNode;
+        cur = cur->right;
+        break;
+      }
+      cur = cur->right;
+    }
+  }
+  
+  while(cur->parent->color == 1){
+    if(cur->parent->left == cur){
+
+    }
+    else{
+
+    }
+
+  }
+
   return t->root;
 }
 
@@ -42,3 +105,12 @@ int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
   // TODO: implement to_array
   return 0;
 }
+
+// int main(){
+//   rbtree *t = new_rbtree();
+//   rbtree_insert(t , 50);
+//   rbtree_insert(t , 10);
+//   rbtree_insert(t , 70);
+//   if(t != NULL)
+//     printf("%d", t->root->color);
+// }
